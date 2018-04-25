4ce90cfadec6a4fa5dc9fff9fd748b2ca67879e0
franco-extreme <franck_ral@hotmail.com>
2018-04-24 22:28:09 -0400

    Initial structure of the project UO Auth Service.

---
+++ src/main/java/com/fral/uo/auth/uoauthservice/UoAuthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fral.uo.auth.uoauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UoAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UoAuthServiceApplication.class, args);
+	}
+}
