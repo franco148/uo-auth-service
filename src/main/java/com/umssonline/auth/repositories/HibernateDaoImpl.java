babbaa15259bf530b61bc57c2241948e3f76166f
franco-extreme <franck_ral@hotmail.com>
2018-05-10 22:00:29 -0400

    AuthService-IntegratePersistenceCore: Working at DAL layer of the service.

--- src/main/java/com/umssonline/auth/repositories/HibernateDaoImpl.java
+++ src/main/java/com/umssonline/auth/repositories/HibernateDaoImpl.java
@@ -3,31 +3,46 @@ package com.umssonline.auth.repositories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class HibernateDaoImpl implements AuthDao {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private EntityManager entityManager;
+
+
+
     @Override
     public <T> T persist(T entity) {
-        return null;
+        entityManager.persist(entity);
+
+        return entity;
     }
 
     @Override
     public <T> Collection<T> persist(T[] entities) {
-        return null;
+        for (T entity : entities) {
+            entityManager.persist(entity);
+        }
+
+        return Arrays.asList(entities);
     }
 
     @Override
     public <T> Collection<T> load(Class<T> entityClass) {
+        //String selectAllQuery = "SELECT e FROM "
         return null;
     }
 
     @Override
     public <T> T find(Class<T> entityClass, Serializable id) {
-        return null;
+        return entityManager.find(entityClass, id);
     }
 
     @Override
