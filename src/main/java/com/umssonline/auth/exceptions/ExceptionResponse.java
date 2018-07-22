0e915ea83fe72e96d32cea1b355578e97ded184d
franco-extreme <franck_ral@hotmail.com>
2018-07-22 17:31:46 -0400

    AuthService-ErrorHandling: ExceptionResponse and Exception Handler class have been implemented.

---
+++ src/main/java/com/umssonline/auth/exceptions/ExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.umssonline.auth.exceptions;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ExceptionResponse {
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+
+    public ExceptionResponse(Date timestamp, String message, String details) {
+
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+}
