ba27cf21b3d3c7b62ce85b2cf0c1aaea2a5dd8dd
franco-extreme <franck_ral@hotmail.com>
2018-07-23 16:28:37 -0400

    AuthService-RestApiImplementation: Fixed and refactored some method definitions in the Controller.

--- src/main/java/com/umssonline/auth/controllers/UserRestController.java
+++ src/main/java/com/umssonline/auth/controllers/UserRestController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.Collection;
+import java.util.Collections;
 
 @RefreshScope
 @RequestMapping("/users")
@@ -29,19 +30,24 @@ public class UserRestController {
 
 
     @GetMapping
-    public ResponseEntity getAll() {
+    public ResponseEntity<Collection<User>> getAll() {
         Collection<User> userCollection = userService.loadAll();
+
+        if (userCollection.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(Collections.emptyList());
+        }
+
         return ResponseEntity.ok(userCollection);
     }
 
     @GetMapping("/{user_id}")
-    public ResponseEntity getById(@PathVariable("user_id") final Long id) {
+    public ResponseEntity<User> getById(@PathVariable("user_id") final Long id) {
         User user = userService.detail(id);
         return ResponseEntity.ok(user);
     }
 
     @PostMapping
-    public ResponseEntity save(@Valid @RequestBody final RegisterUserDto user) {
+    public ResponseEntity<User> save(@Valid @RequestBody final RegisterUserDto user) {
         User converted = modelMapper.map(user, User.class);
 
         User persistedUser = userService.register(converted);
@@ -49,7 +55,7 @@ public class UserRestController {
     }
 
     @PatchMapping("/{user_id}")
-    public ResponseEntity edit(@PathVariable("user_id") final Long id, @Valid @RequestBody final UpdateUserDto user) throws EntityNotFoundException {
+    public ResponseEntity<User> edit(@PathVariable("user_id") final Long id, @Valid @RequestBody final UpdateUserDto user) throws EntityNotFoundException {
         User converted = modelMapper.map(user, User.class);
         converted.setId(id);
 
@@ -58,28 +64,28 @@ public class UserRestController {
     }
 
     @DeleteMapping("/{user_id}")
-    public ResponseEntity delete(@PathVariable("user_id") final Long id) {
+    public ResponseEntity<Boolean> delete(@PathVariable("user_id") final Long id) {
         boolean wasUnregistered = userService.unregister(id);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(wasUnregistered);
+        return ResponseEntity.status(HttpStatus.OK).body(wasUnregistered);
     }
 
     @PostMapping("/login")
-    public ResponseEntity logIn(@Valid @RequestBody final CredentialsDto credentialsDto) throws EntityNotFoundException {
+    public ResponseEntity<User> logIn(@Valid @RequestBody final CredentialsDto credentialsDto) throws EntityNotFoundException {
         User loggedUser = userService.login(credentialsDto.getAccount(), credentialsDto.getPassword());
 
         return ResponseEntity.ok(loggedUser);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity logout(@Valid @RequestBody final CredentialsDto credentialsDto) throws EntityNotFoundException {
+    public ResponseEntity<Boolean> logout(@Valid @RequestBody final CredentialsDto credentialsDto) throws EntityNotFoundException {
         boolean wasLogout = userService.logout(credentialsDto.getAccount(), credentialsDto.getPassword());
 
         return ResponseEntity.ok(wasLogout);
     }
 
     @PostMapping("/confirm/{user_id}")
-    public ResponseEntity confirmSubscription(@PathVariable("user_id") final Long id) {
+    public ResponseEntity<Void> confirmSubscription(@PathVariable("user_id") final Long id) {
         userService.confirmSubscription(id);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
