7bfd1249203525fea03c8352fe4e6cda6e971875
franco-extreme <franck_ral@hotmail.com>
2018-07-23 17:04:36 -0400

    AuthService-RestApiImplementation: Added API documentation to the application DTOs.

--- src/main/java/com/umssonline/auth/models/dto/RegisterRoleDto.java
+++ src/main/java/com/umssonline/auth/models/dto/RegisterRoleDto.java
@@ -2,15 +2,24 @@ package com.umssonline.auth.models.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.umssonline.auth.models.entity.RoleEnum;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
+@ApiModel(description = "Role Model")
 @Getter
 @Setter
 public class RegisterRoleDto {
 
+    @ApiModelProperty
+    (
+        notes = "Authority field can not be null or empty.",
+        required = true,
+        allowableValues = "ADMIN, TRIBUNAL, DIRECTOR, PROFESSOR, TUTOR, STUDENT"
+    )
     @NotNull
     private RoleEnum authority;
 
--- src/main/java/com/umssonline/auth/models/dto/UpdateRoleDto.java
+++ src/main/java/com/umssonline/auth/models/dto/UpdateRoleDto.java
@@ -1,18 +1,33 @@
 package com.umssonline.auth.models.dto;
 
 import com.umssonline.auth.models.entity.RoleEnum;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
+@ApiModel(description = "Role Model")
 @Getter
 @Setter
 public class UpdateRoleDto {
 
+    @ApiModelProperty
+    (
+        notes = "Id field can not be null or empty.",
+        required = true,
+        dataType = "Integer"
+    )
     @NotNull
     private Long id;
 
+    @ApiModelProperty
+    (
+        notes = "Authority field can not be null or empty.",
+        required = true,
+        allowableValues = "ADMIN, TRIBUNAL, DIRECTOR, PROFESSOR, TUTOR, STUDENT"
+    )
     @NotNull
     private RoleEnum authority;
 }
