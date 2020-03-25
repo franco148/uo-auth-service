0ecb7c4e81ffb74e480678ee13f3d5f8f8b9fb48
franco-extreme <franck_ral@hotmail.com>
2020-03-25 12:54:37 -0400

    AuthService-SecurityJwt: Enabled security to Auth Microservice - AuthenticationFilter Implementing attemptAuthentication()

--- src/main/java/com/umssonline/auth/config/SecurityConfig.java
+++ src/main/java/com/umssonline/auth/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.umssonline.auth.config;
 
+import com.umssonline.auth.filter.AuthenticationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,8 +14,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/users/**").permitAll()
 //        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("uo.auth.gateway.ip")); // When we want to specify the source address
+        .and().addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+        return authenticationFilter;
     }
 }
---
+++ src/main/java/com/umssonline/auth/filter/AuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.umssonline.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.umssonline.auth.controller.dto.request.CredentialsDto;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            CredentialsDto credentials = new ObjectMapper()
+                                         .readValue(request.getInputStream(), CredentialsDto.class);
+
+            return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    credentials.getAccount(),
+                    credentials.getPassword(),
+                    new ArrayList<>()
+                )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
